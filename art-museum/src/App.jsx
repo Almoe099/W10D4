262a4751a79e0e82929bcfcb68a0f80d8cb68cdf
Almoe099 <almoe9@LAPTOP-I8VP6L2K>
2023-12-21 15:06:16 -0500

    completed phase 2

---
+++ art-museum/src/App.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import harvardArt from './data/harvardArt';
+import GalleryNavigation from './components/GalleryNavigation';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/galleries",
+    element: <GalleryNavigation galleries={harvardArt.records}/>,
+    // element: <> <h2>Harvard Art Museum</h2> <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p> </>,
+  },
+  { 
+    path: "*",
+    element: <h2>Page Not Found</h2>,
+  },
+]);
+
+function App() {
+  // console.log(harvardArt);
+  return <RouterProvider router={router}/>;
+}
+
+export default App;
